2b2ceb4de62d1eaef96551dcc8a81ce5a04108ed
AlexeyKapralov <alewka24@gmail.com>
2024-03-30 14:51:50 +0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, {Request, Response} from 'express'
+import cors from 'cors'
 
 export const app = express()
 app.use(express.json())
+
+app.use(cors())
 
 
 // const products = [{title: 'tomato'}, {title: 'orange'}]
